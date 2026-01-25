21a1ab64b3dc7e4023133fb653d6d2a17f701bfe
Akhil <akhil_prasenan@outlook.com>
2026-01-25 15:01:01 +0800

    chore: configure for github pages deployment

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createHashRouter } from "react-router-dom"
 import { RootLayout } from "./app/layout/RootLayout"
 import Home from "./app/routes/home"
 import CaseStudies from "./app/routes/case-studies"
 import CaseStudyDetail from "./app/routes/case-study.$slug"
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <RootLayout />,
